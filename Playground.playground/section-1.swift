d56b4b3e7f43785a23a857789b5c741055223e81
tikzero <istudents@iClassrooms-iMac-4.local>
2015-01-16 16:24:25 +0700

    Lab1-2
    Playground

---
+++ Playground.playground/section-1.swift
@@ -0,0 +1,26 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let tutorialTeam = 56
+let editorialTeam = 23
+let totalTeam = tutorialTeam + editorialTeam
+
+//totalTeam +=1
+let priceIntInferred = -19
+let priceIntExplicit:Int = -19
+
+let priceIntferred = -19.99
+let priceExplicit:Double = -19.99
+
+
+let priceFloatInferred = -19.99
+let priceFloatExplicit:Float = -19.99
+
+let onSaleInferred = true
+let onSaleExplicit:Bool = false
+
+let nameInferred = "Whoopie Cushion"
+let nameExplicit:String = "Whoopie Cushion"
