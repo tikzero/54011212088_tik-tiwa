5e82c2e32cd099ae4e18438e4331ed3946b7fe74
tikzero <istudents@iClassrooms-iMac-5.local>
2015-04-03 17:04:06 +0700

    Animation_Lab7#2
    Animation_Lab7#2

--- Animation_Lab7#2/Animation_Lab7#2/ViewController.swift
+++ Animation_Lab7#2/Animation_Lab7#2/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     let container = UIView()
     let redSquare = UIView()
     let blueSquare = UIView()
+    let yelloSquare = UIView()
+    
     
 
     @IBAction func animateButtonTapped(sender: AnyObject) {
@@ -26,10 +28,60 @@ class ViewController: UIViewController {
         views = (frontView: self.blueSquare, backView: self.redSquare)
         }
         
-        let transitionOptions = UIViewAnimationOptions.TransitionFlipFromLeft
+        let transitionOptions = UIViewAnimationOptions.TransitionCurlUp
         UIView.transitionFromView(views.frontView, toView: views.backView, duration: 1.0, options: transitionOptions, completion: nil)
         
     }
+    
+    @IBAction func animateButtonTapped2(sender: AnyObject) {
+        var views: (frontView: UIView, backView: UIView)
+        
+        
+        if((self.redSquare.superview) != nil){
+            views = (frontView: self.redSquare, backView: self.blueSquare)
+        }
+        else{
+            views = (frontView: self.blueSquare, backView: self.redSquare)
+        }
+        
+        let transitionOptions = UIViewAnimationOptions.TransitionCurlDown
+        
+        UIView.transitionFromView(views.frontView, toView: views.backView, duration: 1.0, options: transitionOptions, completion: nil)
+    }
+    
+    @IBAction func animateButtonTapped3(sender: AnyObject) {
+        var views: (frontView: UIView, backView: UIView)
+        
+        
+        if((self.redSquare.superview) != nil){
+            views = (frontView: self.redSquare, backView: self.blueSquare)
+        }
+        else{
+            views = (frontView: self.blueSquare, backView: self.redSquare)
+        }
+        
+        let transitionOptions = UIViewAnimationOptions.TransitionFlipFromLeft
+        
+        UIView.transitionFromView(views.frontView, toView: views.backView, duration: 1.0, options: transitionOptions, completion: nil)
+    }
+    
+    @IBAction func animateButtonTapped4(sender: AnyObject) {
+        var views: (frontView: UIView, backView: UIView)
+        
+        
+        if((self.redSquare.superview) != nil){
+            views = (frontView: self.redSquare, backView: self.blueSquare)
+        }
+        else{
+            views = (frontView: self.blueSquare, backView: self.redSquare)
+        }
+        
+        let transitionOptions = UIViewAnimationOptions.TransitionFlipFromRight
+        
+        UIView.transitionFromView(views.frontView, toView: views.backView, duration: 1.0, options: transitionOptions, completion: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
          //Do any additional setup after loading the view, typically from a nib.
@@ -41,9 +93,14 @@ class ViewController: UIViewController {
         
         self.redSquare.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
         self.blueSquare.frame = redSquare.frame
+        //self.yelloSquare.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        self.yelloSquare.frame = redSquare.frame
+        
+        
         
         self.redSquare.backgroundColor = UIColor.redColor()
         self.blueSquare.backgroundColor = UIColor.blueColor()
+        self.yelloSquare.backgroundColor = UIColor.yellowColor()
         
         self.container.addSubview(self.redSquare)
         
